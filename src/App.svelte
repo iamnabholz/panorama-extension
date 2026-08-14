b5504d3a4b8f166ef10e73c1f98a90ab73604ba9
Lukas Nabholz <lukas@nabholz.work>
2026-08-14 12:04:00 -0400

    Start 3.0

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,89 @@
 <script>
-  import Background from "./lib/Background.svelte";
-  import Reddit from "./lib/Reddit.svelte";
-  import Weather from "./lib/Weather.svelte";
-  import Shorcuts from "./lib/Shorcuts.svelte";
-  import { settings } from "./stores";
-  import { slide } from "svelte/transition";
-  import Today from "./lib/Today.svelte";
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import heroImg from './assets/hero.png'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <div class="side-bar">
-    {#if $settings.dateActive}
-      <span transition:slide>
-        <Today />
-      </span>
-    {/if}
-    {#if $settings.weatherActive}
-      <span transition:slide>
-        <Weather />
-      </span>
-    {/if}
-    {#if $settings.sitesActive}
-      <span transition:slide>
-        <Shorcuts />
-      </span>
-    {/if}
+<section id="center">
+  <div class="hero">
+    <img src={heroImg} class="base" width="170" height="179" alt="" />
+    <img src={svelteLogo} class="framework" alt="Svelte logo" />
+    <img src={viteLogo} class="vite" alt="Vite logo" />
   </div>
+  <div>
+    <h1>Get started</h1>
+    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
+  </div>
+  <Counter />
+</section>
 
-  {#if $settings.redditActive}
-    <Reddit />
-  {/if}
-</main>
+<div class="ticks"></div>
 
-<Background />
+<section id="next-steps">
+  <div id="docs">
+    <svg class="icon" role="presentation" aria-hidden="true">
+      <use href="/icons.svg#documentation-icon"></use>
+    </svg>
+    <h2>Documentation</h2>
+    <p>Your questions, answered</p>
+    <ul>
+      <li>
+        <a href="https://vite.dev/" target="_blank" rel="noreferrer">
+          <img class="logo" src={viteLogo} alt="" />
+          Explore Vite
+        </a>
+      </li>
+      <li>
+        <a href="https://svelte.dev/" target="_blank" rel="noreferrer">
+          <img class="button-icon" src={svelteLogo} alt="" />
+          Learn more
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div id="social">
+    <svg class="icon" role="presentation" aria-hidden="true">
+      <use href="/icons.svg#social-icon"></use>
+    </svg>
+    <h2>Connect with us</h2>
+    <p>Join the Vite community</p>
+    <ul>
+      <li>
+        <a href="https://github.com/vitejs/vite" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#github-icon"></use>
+          </svg>
+          GitHub
+        </a>
+      </li>
+      <li>
+        <a href="https://chat.vite.dev/" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#discord-icon"></use>
+          </svg>
+          Discord
+        </a>
+      </li>
+      <li>
+        <a href="https://x.com/vite_js" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#x-icon"></use>
+          </svg>
+          X.com
+        </a>
+      </li>
+      <li>
+        <a href="https://bsky.app/profile/vite.dev" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#bluesky-icon"></use>
+          </svg>
+          Bluesky
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
 
-<style>
-  main {
-    padding: 6vh 2vh;
-    height: 100vh;
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
-    column-gap: 4rem;
-  }
-
-  .side-bar {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-  }
-
-  .side-bar span {
-    padding-bottom: 2vh;
-  }
-
-  @media screen and (max-width: 680px) {
-    main {
-      flex-direction: column;
-    }
-
-    .side-bar {
-      width: 100%;
-    }
-  }
-</style>
+<div class="ticks"></div>
+<section id="spacer"></section>
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from "vite";
-import {svelte} from "@sveltejs/vite-plugin-svelte";
-import webExtension from "vite-plugin-web-extension";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), webExtension({browser: "chrome"})],
-});
